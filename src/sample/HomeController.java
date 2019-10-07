26e3e9fe0d2ea6bf72270d456d45e91b1445f5a6
Anton Zhukov <Anton-Zhukov@outlook.com>
2019-10-07 18:48:51 +0300

    Добавил вывод строковой инфы

--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -2,20 +2,21 @@ package sample;
 
 import java.net.URL;
 import java.util.Date;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DBManager;
 import db.entity.Report;
 import db.entity.Reports;
+import db.entity.Teacher;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
-
-import javax.swing.*;
 
 public class HomeController {
 
@@ -41,13 +42,10 @@ public class HomeController {
     private Button importInExcelButton;
 
     @FXML
-    private ScrollBar scrollBaR;
-
-    @FXML
     private TableView<Reports> tableViewReportsTable2;
 
     @FXML
-    private TableColumn<Reports, Long> DepartmentNameInTable;
+    private TableColumn<Reports, String> DepartmentNameInTable;
 
     @FXML
     private TableColumn<Reports, String> ProjectNameInTable;
@@ -56,10 +54,10 @@ public class HomeController {
     private TableView<Report> tableViewReportsTable;
 
     @FXML
-    private TableColumn<?, ?> numberColumnInTable;
+    private TableColumn<Report, Integer> numberColumnInTable;
 
     @FXML
-    private TableColumn<Report, Integer> NameColumnEventInTable;
+    private TableColumn<Report, String> NameColumnEventInTable;
 
     @FXML
     private TableColumn<Report, Date> dataColumnInTable;
@@ -68,19 +66,14 @@ public class HomeController {
     private TableColumn<Report, String> rateColumnInTable;
 
     @FXML
-    private TableColumn<Report, Integer> authorColumnInTable;
+    private TableColumn<Report, String> authorColumnInTable;
 
     @FXML
-    private TableColumn<?, ?> NameColumnEventInTable1;
-
-    @FXML
-    void initialize(){
-
+    void initialize() {
         showAllReports.setOnAction(event -> {
-            if (isNullOrEmpty(groupNumber.getText())){
+            if (isNullOrEmpty(groupNumber.getText())) {
                 findAllReports();
-            }
-            else {
+            } else {
                 findReportsByDep();
             }
         });
@@ -88,49 +81,87 @@ public class HomeController {
             TableRow<Reports> row = new TableRow<>();
             row.setOnMouseClicked(event -> {
                 if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY
-                        && event.getClickCount() == 2) {
+                            && event.getClickCount() == 2) {
                     Reports clickedRow = row.getItem();
-                    findReport((int)clickedRow.getId());
+                    findReport((int) clickedRow.getId());
                 }
             });
-            return row;});
+            return row;
+        });
+
+        NameColumnEventInTable
+                    .setCellValueFactory(new PropertyValueFactory<Report, String>("activity_id") {
+                        @Override
+                        public ObservableValue<String> call(
+                                    CellDataFeatures<Report, String> param) {
+                            Object value = super.call(param).getValue();
+                            String data = DBManager.getInstance().getActivityById((Long) value)
+                                        .getName();
+                            ObservableValue<String> newValue = new ReadOnlyObjectWrapper<String>(
+                                        data);
+                            return newValue;
+                        }
+                    });
+        dataColumnInTable.setCellValueFactory(new PropertyValueFactory<>("date"));
+        rateColumnInTable.setCellValueFactory(new PropertyValueFactory<>("status"));
+        authorColumnInTable
+                    .setCellValueFactory(new PropertyValueFactory<Report, String>("user_id") {
+                        @Override
+                        public ObservableValue<String> call(
+                                    CellDataFeatures<Report, String> param) {
+                            Object value = super.call(param).getValue();
+                            for (Teacher teacher : DBManager.getInstance().getAllTeachers()) {
+                                if (teacher.getId() == (Long) value) {
+                                    return new ReadOnlyObjectWrapper<String>(teacher.getName());
+                                }
+                            }
+                            return new ReadOnlyObjectWrapper<String>("");
+                        }
+                    });
+        numberColumnInTable.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        DepartmentNameInTable.setCellValueFactory(
+                    new PropertyValueFactory<Reports, String>("department_id") {
+                        @Override
+                        public ObservableValue<String> call(
+                                    CellDataFeatures<Reports, String> param) {
+                            Object value = super.call(param).getValue();
+                            String data = DBManager.getInstance().getDepartmentById((Long) value)
+                                        .getName();
+                            ObservableValue<String> newValue = new ReadOnlyObjectWrapper<String>(
+                                        data);
+                            return newValue;
+                        }
+                    });
+        ProjectNameInTable.setCellValueFactory(new PropertyValueFactory<>("name"));
     }
-   // private ObservableList<Reports> reports = FXCollections.observableArrayList();
 
     private boolean isNullOrEmpty(String str) {
         return str == null || str.trim().length() == 0;
     }
 
-    private void findReportsByDep(){
+    private void findReportsByDep() {
         int department = Integer.parseInt(groupNumber.getText());
         DBManager manager = DBManager.getInstance();
-        ObservableList<Reports> reports =  FXCollections.observableArrayList(manager.getReportsByDepartment(department));
-
-        DepartmentNameInTable.setCellValueFactory(new PropertyValueFactory<>("department_name"));
-        ProjectNameInTable.setCellValueFactory(new PropertyValueFactory<>("name"));
-
+        ObservableList<Reports> reports = FXCollections
+                    .observableArrayList(manager.getReportsByDepartment(department));
         tableViewReportsTable2.setItems(reports);
 
     }
 
-    private void findAllReports(){
+    private void findAllReports() {
         DBManager manager = DBManager.getInstance();
-        ObservableList<Reports> reports =  FXCollections.observableArrayList(manager.getAllReports());
-        DepartmentNameInTable.setCellValueFactory(new PropertyValueFactory<>("department_name"));
-        ProjectNameInTable.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ObservableList<Reports> reports = FXCollections
+                    .observableArrayList(manager.getAllReports());
         tableViewReportsTable2.setItems(reports);
 
     }
 
-    private void findReport(int id){
+    private void findReport(int id) {
         DBManager manager = DBManager.getInstance();
-        ObservableList<Report> report = FXCollections.observableArrayList(manager.getReportById(id));
+        ObservableList<Report> report = FXCollections
+                    .observableArrayList(manager.getReportById(id));
         tableViewReportsTable.setItems(report);
-        NameColumnEventInTable.setCellValueFactory(new PropertyValueFactory<>("activity_id"));
-        dataColumnInTable.setCellValueFactory(new PropertyValueFactory<>("date"));
-        rateColumnInTable.setCellValueFactory(new PropertyValueFactory<>("status"));
-        authorColumnInTable.setCellValueFactory(new PropertyValueFactory<>("user_id"));
-
     }
 
 }
